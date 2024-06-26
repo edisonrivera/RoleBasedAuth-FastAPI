c3ffb5af2c82201bf23f33c5937f2c8c3254e3a6
EdisonR02 <riveraeddy58@gmail.com>
2024-06-26 17:07:11 -0500

    Fix: Functions names

--- app/routers/v1/test.py
+++ app/routers/v1/test.py
@@ -7,7 +7,7 @@ from fastapi import APIRouter, Depends
 test = APIRouter(prefix="/test", tags=["Test"])
 
 
-@test.get("/user", dependencies=[Depends(PreAuthorize(allowed_roles=[RoleEnum.USER, RoleEnum.SUPPORT], strict_roles=True))], response_model=ResponseModel[str], 
+@test.get("/user", dependencies=[Depends(PreAuthorize(allowed_roles=[RoleEnum.USER, RoleEnum.SUPPORT], strict_roles=True))], response_model=ResponseModel[str],
           response_model_exclude_none=True)
 async def user():
     return ResponseModel[str](message="User Dashboard")
@@ -15,11 +15,11 @@ async def user():
 
 @test.get("/admin", dependencies=[Depends(PreAuthorize(allowed_roles=[RoleEnum.ADMIN]))], response_model=ResponseModel[str],
           response_model_exclude_none=True)
-async def user():
+async def admin():
     return ResponseModel[str](message="Admin Dashboard")
 
 
 @test.get("/support", dependencies=[Depends(PreAuthorize(allowed_roles=[RoleEnum.SUPPORT]))], response_model=ResponseModel[str],
           response_model_exclude_none=True)
-async def user():
+async def support():
     return ResponseModel[str](message="Support Dashboard")
